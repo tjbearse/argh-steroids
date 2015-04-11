80f877c58348c7e83efb61924f87b231f3f5ebc4
TJ <bearse.thomas@gmail.com>
2015-04-11 13:39:16 -0400

    fix angle calc

--- ship_drivers/starter.py
+++ ship_drivers/starter.py
@@ -10,10 +10,19 @@ def dist(entity, other):
     return ((entity.x - other.x)**2 + (entity.y - other.y)**2)**(1/2)
 
 def getAngle(entity, other):
-    # needs adjusting
-    x = entity.x - other.x
-    y = entity.y - other.y
-    return math.degrees(math.atan2(y, x))
+    # gives the angle of the vector from entity to other
+    # wrt to the positive x axis (but clockwise, counter intuitive but coords are upside down)
+    # e.g.
+    #               315
+    #             .
+    #           .
+    #  180 . . x . . 0
+    #           .
+    #             .
+    #               45
+    x = other.x - entity.x
+    y = other.y - entity.y
+    return (math.degrees(math.atan2(y, x)) + 360) % 360
     
 
 r = gs.Response()
@@ -25,7 +34,7 @@ while True:
 
     close = closest(state.ship, state.asteroids)
     angle = getAngle(state.ship, close)
-    #print >>sys.stderr, ((state.ship.x, state.ship.y), (close.x, close.y), angle(state.ship, close))
+    print >>sys.stderr, ((state.ship.x, state.ship.y), (close.x, close.y), getAngle(state.ship, close))
 
     # set command
     r.set_fire(False)
